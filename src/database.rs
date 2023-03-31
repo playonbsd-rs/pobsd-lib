e3ab77ea730a1bf2bd5bed2185a1df2878b41e09
Hukadan <me@hukadan.org>
2023-03-31 08:57:48 +0200

    Factorize the macro rules

--- src/database.rs
+++ src/database.rs
@@ -4,6 +4,31 @@ use paste::paste;
 use pobsd_parser::Game;
 use std::collections::HashMap;
 
+macro_rules! load_game {
+    (items: $($item:ident),+; arrays: $($array:ident),+) => {
+        paste! {
+            /// Load the given game in the database
+            pub fn load_game(&mut self, game: Game) {
+                let uid = game.uid;
+                self.add_game(game);
+                let game = self.games.get(&uid).unwrap().clone();
+                $(
+                    if let Some(item) = &game.$item {
+                        self.[<add_game_to_ $item s>](&item, game.uid);
+                    }
+                )*
+                $(
+                    if let Some(items) = &game.$array {
+                        for item in items {
+                            self.[<add_game_to_ $array>](&item, game.uid);
+                        }
+                    }
+                )*
+            }
+        }
+    };
+}
+
 macro_rules! add_game_to {
     ($field:ident) => {
         paste! {
@@ -13,25 +38,6 @@ macro_rules! add_game_to {
                     None => {
                         let _ = self.[<$field>].insert(item.into(), vec![game_id]);
                     }
-                }
-            }
-        }
-    };
-}
-
-macro_rules! push {
-    ($game:ident, $field:ident, $db:ident ) => {
-        paste! {
-            if let Some(item) = &$game.$field {
-                $db.[<add_game_to_ $field s>](&item, $game.uid);
-            }
-        }
-    };
-    ($game:ident, array $field:ident, $db:ident) => {
-        paste! {
-            if let Some(items) = &$game.$field {
-                for item in items {
-                    $db.[<add_game_to_ $field>](&item, $game.uid);
                 }
             }
         }
@@ -69,23 +75,15 @@ impl GameDataBase {
         }
         db
     }
-    /// Load the given game in the database
-    pub fn load_game(&mut self, game: Game) {
-        let uid = game.uid;
-        self.add_game(game);
-        let game = self.games.get(&uid).unwrap().clone();
+    load_game!(
+        items: engine, runtime, year, dev, publi;
+        arrays: genres, tags
+    );
 
-        push!(game, engine, self);
-        push!(game, runtime, self);
-        push!(game, array genres, self);
-        push!(game, array tags, self);
-        push!(game, year, self);
-        push!(game, dev, self);
-        push!(game, publi, self);
-    }
     fn add_game(&mut self, game: Game) {
         self.games.insert(game.uid, game);
     }
+
     add_game_to!(tags);
     add_game_to!(engines);
     add_game_to!(runtimes);
